ab2c63726c5b2b9e1a77e286d88ca7248ebb4f13
Arn.28 <arnold.balabarca.27@unsch.edu.pe>
2023-06-29 23:13:39 -0500

    Added function validator per input and save function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,134 @@
-import { cardNameSanitizer, cardNumberSanitizer } from './inputValidator.js'
-import { nameAutocompletion, cardNumberAutocompletion } from './cardAutocomplete.js'
-const inputCardholderName = cardForm.cardholderName
-const inputCardNumber = cardForm.cardNumber
+import {
+  cardNameSanitizer,
+  cardNumberSanitizer,
+  inputNumberSanitizer,
+  sanitizeAndAutocomplete,
+} from './inputSanitizer.js'
+import {
+  nameAutocompletion,
+  cardNumberAutocompletion,
+  monthAutocompletion,
+  yearAutocompletion,
+  cvcAutocompletion,
+  hiddeCartDetails,
+  showThankYouPanel,
+} from './cardAutocomplete.js'
+import {
+  hasValue,
+  inputValidator,
+  isAllFieldComplete,
+  isAllFieldValid,
+  isCardNumberLengthCorrect,
+  isCvcLengthCorrect,
+} from './formValidation.js'
+const cardholderNameInput = cardForm.cardholderName
+const cardNumberInput = cardForm.cardNumber
+const expMonthInput = cardForm.expMonth
+const expYearInput = cardForm.expYear
+const cvcInput = cardForm.cvc
+const submitButton = cardForm.submit
 
-inputCardholderName.addEventListener('input', ({ target }) => {
-  inputCardholderName.value = cardNameSanitizer(target.value)
-  nameAutocompletion(inputCardholderName.value)
+const errorMessage = {
+  BLANK: `Can't be blank`,
+  FORMAT: `Wrong format`,
+}
+
+sanitizeAndAutocomplete({
+  input: cardholderNameInput,
+  sanitizer: cardNameSanitizer,
+  autocomplete: nameAutocompletion,
 })
 
-inputCardNumber.addEventListener('input', ({ target }) => {
-  inputCardNumber.value = cardNumberSanitizer(target.value)
-  cardNumberAutocompletion(inputCardNumber.value)
+inputValidator({
+  input: cardholderNameInput,
+  validator: hasValue,
+  errorMessage: errorMessage.BLANK,
+})
+
+sanitizeAndAutocomplete({
+  input: cardNumberInput,
+  sanitizer: cardNumberSanitizer,
+  autocomplete: cardNumberAutocompletion,
+})
+
+cardNumberInput.addEventListener(
+  'blur',
+  () => {
+    cardNumberInput.value.length > 0
+      ? inputValidator({
+          input: cardNumberInput,
+          validator: isCardNumberLengthCorrect,
+          errorMessage: errorMessage.FORMAT,
+        })
+      : inputValidator({
+          input: cardNumberInput,
+          validator: hasValue,
+          errorMessage: errorMessage.BLANK,
+        })
+  },
+  true,
+)
+
+sanitizeAndAutocomplete({
+  input: expMonthInput,
+  sanitizer: inputNumberSanitizer,
+  autocomplete: monthAutocompletion,
+})
+
+inputValidator({
+  input: expMonthInput,
+  validator: hasValue,
+  errorMessage: errorMessage.BLANK,
+})
+
+sanitizeAndAutocomplete({
+  input: expYearInput,
+  sanitizer: inputNumberSanitizer,
+  autocomplete: yearAutocompletion,
+})
+
+inputValidator({
+  input: expYearInput,
+  validator: hasValue,
+  errorMessage: errorMessage.BLANK,
+})
+
+sanitizeAndAutocomplete({
+  input: cvcInput,
+  sanitizer: inputNumberSanitizer,
+  autocomplete: cvcAutocompletion,
+})
+
+cvcInput.addEventListener(
+  'blur',
+  () => {
+    cvcInput.value.length > 0
+      ? inputValidator({
+          input: cvcInput,
+          validator: isCvcLengthCorrect,
+          errorMessage: errorMessage.FORMAT,
+        })
+      : inputValidator({
+          input: cvcInput,
+          validator: hasValue,
+          errorMessage: errorMessage.BLANK,
+        })
+  },
+  true,
+)
+
+submitButton.addEventListener('click', (e) => {
+  const cardPayload = {
+    cardholderName: cardholderNameInput.value,
+    cardNumber: cardNumberInput.value,
+    expMonth: expMonthInput.value,
+    expYear: expYearInput.value,
+    cvc: cvcInput.value,
+  }
+  e.preventDefault()
+  if (isAllFieldComplete() && isAllFieldValid()) {
+    console.log('cardPayload =>', cardPayload)
+    hiddeCartDetails()
+    showThankYouPanel()
+  }
 })
